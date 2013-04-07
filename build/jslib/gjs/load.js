4042cc0ff7cf46955b74ccbd81b6e12bde0c73ca
Denys Digtiar <duemir@gmail.com>
2013-04-07 19:54:05 +1000

    'load' function is implemented.
    It relies on 'eval' because I am not aware of better GJS way to do it.

---
+++ build/jslib/gjs/load.js
@@ -0,0 +1,15 @@
+/**
+ * @license RequireJS Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: http://github.com/jrburke/requirejs for details
+ */
+
+/*jslint strict: false */
+/*global define: false */
+
+define(['./file'], function (file) {
+    function load(fileName) {
+        eval(file.readFile(fileName));
+    }
+    return load;
+});
