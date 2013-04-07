68fbc6b8675c40d61bd7987b3fbf73f125d8bdf0
Denys Digtiar <duemir@gmail.com>
2013-04-07 19:43:18 +1000

    Command line arguments handling is implemented.

---
+++ build/jslib/gjs/args.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: http://github.com/jrburke/requirejs for details
+ */
+
+/*jslint strict: false */
+/*global define: false, ARGV: false */
+
+define(function () {
+    var args = ARGV;
+
+    //Ignore any command option used for r.js
+    if (args[0] && args[0].indexOf('-' === 0)) {
+        args = args.slice(1);
+    }
+
+    return args;
+});
